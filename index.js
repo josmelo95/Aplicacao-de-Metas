0274c94edf3daac4bcf15f28d798a62d08ca8eaa
josmelo95 <you@example.com>
2024-09-11 16:14:43 -0300

    após aula 2

--- index.js
+++ index.js
@@ -1,48 +1,211 @@
-// olá, mundo!
-console.log("olá, mundo!")
+const { select, input, checkbox } = require('@inquirer/prompts')
+const fs = require("fs").promises
 
+let mensagem = "Bem vindo ao App de Metas";
 
-// variáveis - let
-let mensagens = "olá, mundo!"
+let metas
 
-
-// variáveis - const
-const mensagem = "olá, eu"
-{
-    const mensagem = "olá, Mayk!"
-    console.log(mensagem)
-}
-console.log(mensagem);
-
-
-// arrays
-let metas = ['mayk', 'alô']
-let metas01 = [2, 'mayk']
-// concatenando valores
-console.log(metas[1] + ", " + metas[0]) 
-
-let metas02 = [
-    metas,
-    {
-        value: 'caminhar 20 minutos todos os dias',
-        checked: false
+const carregarMetas = async () => {
+    try {
+        const dados = await fs.readFile("metas.json", "utf-8")
+        metas = JSON.parse(dados)
     }
-]
-console.log(metas02[1].value);
-
-// objetos
-let meta = {
-    value: 'ler um livro por mês',
-    address: 2,
-    checked: true,
-    isChecked: () => {
-        console.log(info)
+    catch (erro) {
+        metas = []
     }
 }
-console.log(meta.value);
 
-// function // arrow function
-const criarMeta = () => {}
+const salvarMetas = async () => {
+    await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+}
 
-// named function
-function criarMetas() {}
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a meta:" })
+
+    if (meta.length == 0) {
+        mensagem = 'A meta não pode ser vazia.'
+        return
+    }
+
+    metas.push(
+        { value: meta, checked: false }
+    )
+    mensagem = "Meta cadastrada com sucesso!"
+}
+
+const listarMetas = async () => {
+    if (metas.length == 0) {
+        mensagem = "Não existem metas!"
+        return
+    }
+
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    if (respostas.length == 0) {
+        mensagem = "Nenhuma meta selecionada!"
+        return
+    }
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    mensagem = 'Meta(s) marcada(s) como concluída(s)'
+
+}
+
+const metasRealizadas = async () => {
+    if (metas.length == 0) {
+        mensagem = "Não existem metas!"
+        return
+    }
+
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if (realizadas.length == 0) {
+        mensagem = 'Não existem metas realizadas! :('
+        return
+    }
+
+    await select({
+        message: "Metas Realizadas: " + realizadas.length,
+        choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    if (metas.length == 0) {
+        mensagem = "Não existem metas!"
+        return
+    }
+
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if (abertas.length == 0) {
+        mensagem = 'Não existem metas abertas! :)'
+        return
+    }
+
+    await select({
+        message: "Metas Abertas: " + abertas.length,
+        choices: [...abertas]
+    })
+}
+
+const deletarMetas = async () => {
+    if(metas.length == 0) {
+        mensagem = "Não existem metas!"
+        return
+    }
+
+    const metasDesmarcadas = metas.map((meta) => {
+        return { value: meta.value, checked: false }
+    })
+
+    const itemsADeletar = await checkbox({
+        message: "Selecione item para deletar",
+        choices: [...metasDesmarcadas],
+        instructions: false,
+    })
+
+    if (itemsADeletar.length == 0) {
+        mensagem = "Nenhum item para deletar!"
+        return
+    }
+
+    itemsADeletar.forEach((item) => {
+        metas = metas.filter((meta) => {
+            return meta.value != item
+        })
+    })
+
+    mensagem = "Meta(s) deleta(s) com sucesso!"
+}
+
+const mostrarMensagem = () => {
+    console.clear();
+
+    if (mensagem != "") {
+        console.log(mensagem)
+        console.log("")
+        mensagem = ""
+    }
+}
+
+const start = async () => {
+    await carregarMetas()
+
+    while (true) {
+        mostrarMensagem()
+        await salvarMetas()
+
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
+                    name: "Deletar metas",
+                    value: "deletar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+        switch (opcao) {
+            case "cadastrar":
+                await cadastrarMeta()
+                break
+            case "listar":
+                await listarMetas()
+                break
+            case "realizadas":
+                await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
+                break
+            case "deletar":
+                await deletarMetas()
+                break
+            case "sair":
+                console.log('Até a próxima!')
+                return
+        }
+    }
+}
+
+start();
--- todo.md
+++ todo.md
@@ -1,7 +1,8 @@
-- Criar metas;
-- Listar metas;
-    - Metas concluídas;
-    - Metas abertas;
-- Marcar/Desmarcar metas como concluídas;
-- Remover metas;
-- Sistema de mensagens.
+[x] Criar metas
+[x] Listar metas
+    [x] Metas realizadas
+    [x] Metas abertas
+[x] Marcar/Desmarcar metas realizadas
+[x] Remover metas
+[x] Sistema de mensages
+[x] Persistir dados
